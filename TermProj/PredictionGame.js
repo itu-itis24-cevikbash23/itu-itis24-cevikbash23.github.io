4027b08f636ba60abe7b3eb5189dba5e2cc714c8
itu-itis24-cevikbash23 <cevikbash24@itu.edu.tr>
2024-12-19 19:20:44 +0300

    Added some functionality to the game
    Added score.The game now keeps the user input.

--- TermProj/PredictionGame.js
+++ TermProj/PredictionGame.js
@@ -1,1 +1,20 @@
-s
+const userInput=document.getElementById("userInput");
+const enterBtn=document.getElementById("enterBtn");
+const score=document.getElementById("score");
+let scorePoint=0
+function wordPrediction(string){
+    if(string.length>1){
+        if(string.toLowerCase()==="unity"){
+            console.log("you win")
+        } else{
+            console.log("you lose")
+        }
+    } else if(string.toLowerCase()==="u"){
+        scorePoint+=20
+        score.innerHTML="Score:"+scorePoint
+    }
+}
+enterBtn.addEventListener("click",() => {
+    const Guess=userInput.value;
+    wordPrediction(Guess)
+})
