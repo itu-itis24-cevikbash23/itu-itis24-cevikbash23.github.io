8b554fb35a7fd559d063f484bb8db508840587cc
itu-itis24-cevikbash23 <cevikbash24@itu.edu.tr>
2024-12-22 12:10:48 +0300

    Updated the prediction game
    Added the main functions of the game

--- TermProj/PredictionGame.js
+++ TermProj/PredictionGame.js
@@ -1,20 +1,102 @@
 const userInput=document.getElementById("userInput");
 const enterBtn=document.getElementById("enterBtn");
+const resetBtn=document.getElementById("resetBtn");
 const score=document.getElementById("score");
+const unknownU=document.getElementById("unknownU");
+const unknownN=document.getElementById("unknownN");
+const unkNownI=document.getElementById("unknownI");
+const unknownT=document.getElementById("unknownU");
+const unknownY=document.getElementById("unknownY");
+const heart1=document.getElementById("heart1");
+const heart2=document.getElementById("heart2");
+const heart3=document.getElementById("heart3");
+let health=3
+let repeatChecker=[]
 let scorePoint=0
-function wordPrediction(string){
-    if(string.length>1){
-        if(string.toLowerCase()==="unity"){
-            console.log("you win")
-        } else{
-            console.log("you lose")
-        }
-    } else if(string.toLowerCase()==="u"){
-        scorePoint+=20
-        score.innerHTML="Score:"+scorePoint
+
+function gameRestart(){
+    health=3
+    repeatChecker=[]
+    scorePoint=0
+    enterBtn.classList.remove("hidden");
+    enterBtn.classList.add("button");
+    resetBtn.classList.remove("hidden");
+    resetBtn.classList.add("button");
+    heart1.src="Health.svg";
+    heart2.src="Health.svg";
+    heart3.src="Health.svg";
+    unknownU.src="QuestionMark.svg"
+    unknownN.src="QuestionMark.svg"
+    unkNownI.src="QuestionMark.svg"
+    unknownT.src="QuestionMark.svg"
+    unknownY.src="QuestionMark.svg"
+}
+
+function gameEnd(){
+    if(health>0){
+        window.alert("You win!");
+    } else{
+        window.alert("You lose!");
+    }
+    enterBtn.classList.remove("button");
+    enterBtn.classList.add("hidden");
+    health=3;
+    repeatChecker=[];
+    scorePoint=0;
+}
+
+function heartBreaker(number){
+    if(number===0){
+        heart1.src="BrokenHealth.svg";  
+    } else if(number===1){
+        heart2.src="BrokenHealth.svg";
+    } else if(number===2){
+        heart3.src="BrokenHealth.svg";
     }
 }
+
+function wordPrediction(string){
+        if(string.toLowerCase()==="unity"){
+            score.innerHTML="Score:"+(100+(health*20));
+        } else{
+            health=0
+            heart1.src="BrokenHealth.svg";
+            heart2.src="BrokenHealth.svg";
+            heart3.src="BrokenHealth.svg";   
+        }
+        gameEnd();
+}
+
+
+function letterPrediction(letter){
+    if(scorePoint===100){
+        gameEnd();
+    }
+    if(letter.toLowerCase()==="u" && !(repeatChecker.includes(letter))){
+        unknownU.src="U.svg";
+        repeatChecker.push(letter);
+        scorePoint+=20;
+        score.innerHTML="Score:"+scorePoint;
+    } else{
+        health=health-1;
+        heartBreaker(health);
+        if(health===0){
+            gameEnd()
+        }
+    }
+}
+    
 enterBtn.addEventListener("click",() => {
-    const Guess=userInput.value;
-    wordPrediction(Guess)
+    resetBtn.classList.remove("hidden");
+    resetBtn.classList.add("button");
+    const guess=userInput.value;
+    if(guess.length>1){
+        wordPrediction(guess);
+        } else{
+            letterPrediction(guess);
+        }
+})
+
+resetBtn.addEventListener("click",() => {
+    gameRestart()
 })
