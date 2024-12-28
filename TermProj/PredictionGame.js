20b422af7d8f85af8e074937fcb0bc59add1e353
itu-itis24-cevikbash23 <cevikbash24@itu.edu.tr>
2024-12-28 22:40:00 +0300

    Updated the index.html and predictiongame.html

--- TermProj/PredictionGame.js
+++ TermProj/PredictionGame.js
@@ -5,7 +5,7 @@ const score=document.getElementById("score");
 const unknownU=document.getElementById("unknownU");
 const unknownN=document.getElementById("unknownN");
 const unkNownI=document.getElementById("unknownI");
-const unknownT=document.getElementById("unknownU");
+const unknownT=document.getElementById("unknownT");
 const unknownY=document.getElementById("unknownY");
 const heart1=document.getElementById("heart1");
 const heart2=document.getElementById("heart2");
@@ -18,10 +18,11 @@ function gameRestart(){
     health=3
     repeatChecker=[]
     scorePoint=0
+    score.innerHTML="Score:00";
     enterBtn.classList.remove("hidden");
     enterBtn.classList.add("button");
-    resetBtn.classList.remove("hidden");
-    resetBtn.classList.add("button");
+    resetBtn.classList.remove("button");
+    resetBtn.classList.add("hidden");
     heart1.src="Health.svg";
     heart2.src="Health.svg";
     heart3.src="Health.svg";
@@ -57,7 +58,12 @@ function heartBreaker(number){
 
 function wordPrediction(string){
         if(string.toLowerCase()==="unity"){
-            score.innerHTML="Score:"+(100+(health*20));
+            unknownU.src="U.svg";
+            unknownN.src="N.svg";
+            unkNownI.src="I.svg";
+            unknownT.src="T.svg";
+            unknownY.src="Y.svg";
+            score.innerHTML="Score:"+(100+(health*20)); //Each remaining health gives extra points
         } else{
             health=0
             heart1.src="BrokenHealth.svg";
@@ -69,20 +75,42 @@ function wordPrediction(string){
 
 
 function letterPrediction(letter){
-    if(scorePoint===100){
-        gameEnd();
-    }
     if(letter.toLowerCase()==="u" && !(repeatChecker.includes(letter))){
         unknownU.src="U.svg";
         repeatChecker.push(letter);
         scorePoint+=20;
         score.innerHTML="Score:"+scorePoint;
-    } else{
+    } else if(letter.toLowerCase()==="n" && !(repeatChecker.includes(letter))){
+        unknownN.src="N.svg";
+        repeatChecker.push(letter);
+        scorePoint+=20;
+        score.innerHTML="Score:"+scorePoint;
+    } else if(letter.toLowerCase()==="i" && !(repeatChecker.includes(letter))){
+        unkNownI.src="I.svg";
+        repeatChecker.push(letter);
+        scorePoint+=20;
+        score.innerHTML="Score:"+scorePoint;
+    } else if(letter.toLowerCase()==="t" && !(repeatChecker.includes(letter))){
+        unknownT.src="T.svg";
+        repeatChecker.push(letter);
+        scorePoint+=20;
+        score.innerHTML="Score:"+scorePoint;
+    } else if(letter.toLowerCase()==="y" && !(repeatChecker.includes(letter))){
+        unknownY.src="Y.svg";
+        repeatChecker.push(letter);
+        scorePoint+=20;
+        score.innerHTML="Score:"+scorePoint;
+    }
+    else{
         health=health-1;
         heartBreaker(health);
         if(health===0){
             gameEnd()
         }
+    }
+    if(scorePoint===100){
+        score.innerHTML="Score:"+(100+(health*20)); //Each remaining health gives extra points
+        gameEnd();
     }
 }
     
